35e35297899058abac2b96ae197d3836233e6aa6
AxelRodas77 <axelrodas.77@gmail.com>
2025-06-27 21:04:23 -0600

    post-detail-done

--- posts/views.py
+++ posts/views.py
@@ -3,7 +3,11 @@ from .models import Post
 from .serializers import PostSerializer
 
 # Create your views here.
-
 class PostList(generics.ListCreateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+# Post detail view
+class PostDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
